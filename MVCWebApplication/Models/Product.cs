e6aab0dda47deb7bb827d3e8ea9ea7ab39588ba1
aanvarov <anvarov2295@gmail.com>
2021-11-04 04:27:07 +0500

    product and category model classes created

---
+++ MVCWebApplication/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace MVCWebApplication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Category ProductCategory { get; set; }
+    }
+}
