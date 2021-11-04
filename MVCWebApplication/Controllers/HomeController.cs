483e0d2e8a22ce3af083c523f213b73a91060a8e
aanvarov <anvarov2295@gmail.com>
2021-11-04 23:15:09 +0500

    home page and edit page created

--- MVCWebApplication/Controllers/HomeController.cs
+++ MVCWebApplication/Controllers/HomeController.cs
@@ -19,10 +19,11 @@ namespace MVCWebApplication.Controllers
         public async Task<ActionResult> Index()
         {
             //Hosted web API REST Service base url
-            const string baseUrl = "http://localhost:5001/";
+            const string baseUrl = "https://localhost:5001/";
 
             var prodInfo = new List<Product>();
             using var client = new HttpClient();
+            Console.WriteLine(client);
             //Passing service base url
             client.BaseAddress = new Uri(baseUrl);
             client.DefaultRequestHeaders.Clear();
@@ -39,9 +40,74 @@ namespace MVCWebApplication.Controllers
                 //Deserializing the response received from web api and storing into the Product list
                 prodInfo = JsonConvert.DeserializeObject<List<Product>>(prResponse);
             }
-
             //returning the Product list to view
             return View(prodInfo);
+        }
+        
+        // GET: Product/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            const string baseurl = "https://localhost:5001/";
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseurl);
+                var res = await client.GetAsync("api/Product/"+id);
+                //Checking the response is successful or not which is sent using HttpClient
+                if (res.IsSuccessStatusCode)
+                {
+                    //Storing the response details received from web api
+                    var prResponse = res.Content.ReadAsStringAsync().Result;
+                    //Deserializing the response received from web api and storing into the Product list
+                        product = JsonConvert.DeserializeObject<Product>(prResponse);
+                }
+                else
+                    ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+            }
+
+            return View(product);
+        }
+        
+        // POST: Product/Edit/5
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, Product prod)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                const string baseurl = "https://localhost:5001/";
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseurl);
+                    var res = await client.GetAsync("api/Product/" + id);
+                    Product product = null;
+                    //Checking the response is successful or not which is sent using HttpClient
+                    if (res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details received from web api
+                        var prResponse = res.Content.ReadAsStringAsync().Result;
+                        //Deserializing the response received from web api and storing into the Product list
+                            product = JsonConvert.DeserializeObject<Product>(prResponse);
+                    }
+                    prod.ProductCategory = product.ProductCategory;
+                    //HTTP POST
+                    var postTask = client.PutAsJsonAsync<Product>("api/Product/"+prod.Id,
+                        prod);
+                    postTask.Wait();
+                    var result = postTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+                //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                //return View(student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public IActionResult Privacy()
--- MVCWebApplication/Startup.cs
+++ MVCWebApplication/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MVCWebApplication.Data;
 
 namespace MVCWebApplication
 {
@@ -24,6 +26,9 @@ namespace MVCWebApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MVCWebApplicationContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("MVCWebApplicationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
