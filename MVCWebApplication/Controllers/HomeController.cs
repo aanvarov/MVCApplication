6c6cf1e8713deb8a3029a8d6f347844ab3e642e7
aanvarov <anvarov2295@gmail.com>
2021-11-04 17:37:50 +0500

    home cotroller get home page function added

--- MVCWebApplication/Controllers/HomeController.cs
+++ MVCWebApplication/Controllers/HomeController.cs
@@ -2,25 +2,46 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVCWebApplication.Models;
+using Newtonsoft.Json;
 
 namespace MVCWebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //GET: Home
+        public async Task<ActionResult> Index()
         {
-            _logger = logger;
-        }
+            //Hosted web API REST Service base url
+            const string baseUrl = "http://localhost:5001/";
 
-        public IActionResult Index()
-        {
-            return View();
+            var prodInfo = new List<Product>();
+            using var client = new HttpClient();
+            //Passing service base url
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Clear();
+            //Define request data format
+            client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+            //Sending request to find web api REST service resource GetAllProducts using HttpClient
+            var res = await client.GetAsync("api/Product");
+            //Checking the response is successful or not which is sent using HttpClient
+            if (res.IsSuccessStatusCode)
+            {
+                //Storing the response details received from web api
+                var prResponse = res.Content.ReadAsStringAsync().Result;
+                //Deserializing the response received from web api and storing into the Product list
+                prodInfo = JsonConvert.DeserializeObject<List<Product>>(prResponse);
+            }
+
+            //returning the Product list to view
+            return View(prodInfo);
         }
 
         public IActionResult Privacy()
