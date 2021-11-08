47fc35290b57d5a788e366eb23d54f66d87d404b
aanvarov <anvarov2295@gmail.com>
2021-11-09 00:46:42 +0500

    install bat deleted directory

--- deployscripts/installwebsite.bat
+++ deployscripts/installwebsite.bat
@@ -1,6 +1,6 @@
 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
-
-IF NOT EXIST C:\webApps\MVCWebApplication mkdir C:\webApps\MVCWebApplication
+IF EXIST C:\webApps\MVCWebApplication rmdir C:\webApps\MVCWebApplication
+mkdir C:\webApps\MVCWebApplication
 
 cd c:\temp
 
